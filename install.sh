e103a7b7b9e5b462a591d618f7031bf85cf42eea
Interceptv <97058207+Interceptv@users.noreply.github.com>
2023-09-16 14:11:44 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-apt update && apt upgrade && wget https://raw.githubusercontent.com/Interceptv/scanny/main/instcheck.sh && chmod 777 instcheck.sh && ./instcheck.sh
+apt update && wget https://raw.githubusercontent.com/Interceptv/scanny/main/instcheck.sh && chmod 777 instcheck.sh && ./instcheck.sh
