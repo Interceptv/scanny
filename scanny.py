abb88d7c6c7e3a9b9c1f96dd652b016cc03098ff
Interceptv <97058207+Interceptv@users.noreply.github.com>
2023-09-16 18:43:25 -0300

    Update scanny.py

--- scanny.py
+++ scanny.py
@@ -60,7 +60,7 @@ if os.path.exists(diretorio):
 else:
     uuid2=("null")
 
-@app.route('/checkUser',methods = ['POST', 'GET'])
+@app.route('/checkUser', methods=['POST', 'GET'])
 def check_user():
     if request.method == 'POST':
         try:
@@ -68,12 +68,49 @@ def check_user():
             user_get = req_data.get("user")
             uuid = uuid2
             username = get_user(user_get)
-            user = get_user(username)
-            if user == "Not exist":
-                return ("{0}\"username\":\"{1}\",\"count_connection\":\"Null\",\"expiration_date\":\"Null\",\"expiration_days\":\"Null\",\"limiter_user\":\"Null\",\"uuid\":\"Null\"{2}" .format(a, user, b))
+            
+            if username == "Not exist":
+                # Se o usuário não for encontrado na primeira verificação, execute este código
+                def extrair_informacoes(username, linha):
+                    partes = linha.strip().split(" | ")
+                    if len(partes) == 3 and partes[1] == username:
+                        return partes[0], partes[2]
+                    else:
+                        return None, None
+
+                # Abrir o arquivo de texto
+                with open("/etc/SSHPlus/RegV2ray", "r") as arquivo:
+                    # Nome de usuário que você deseja pesquisar
+                    username_procurado = user_get
+
+                    # Loop pelas linhas do arquivo
+                    encontrado = False
+                    for linha in arquivo:
+                        uuid, data = extrair_informacoes(username_procurado, linha)
+                        if uuid is not None:
+                            return jsonify({
+                                "username": username_procurado,
+                                "count_connection": cont_online(username_procurado),
+                                "expiration_date": data,
+                                "expiration_days": check_dias(username_procurado),
+                                "limiter_user": limiter_user(username_procurado),
+                                "uuid": uuid
+                            })
+                            encontrado = True
+                            break
+
+                    # Se o nome de usuário não foi encontrado no segundo arquivo, imprime "null"
+                    if not encontrado:
+                        return jsonify({'error': str(e)})
             else:
-                return ("{0}\"username\":\"{1}\",\"count_connection\":\"{2}\",\"expiration_date\":\"{3}\",\"expiration_days\":\"{4}\",\"limiter_user\":\"{5}\",\"uuid\":\"{6}\"{7}" .format(a, username, cont_online(username), check_data(username), check_dias(username), limiter_user(username), 
-uuid2, b))
+                return jsonify({
+                    "username": username,
+                    "count_connection": cont_online(username),
+                    "expiration_date": check_data(username),
+                    "expiration_days": check_dias(username),
+                    "limiter_user": limiter_user(username),
+                    "uuid": uuid2
+                })
         except Exception as e:
             return jsonify({'error': str(e)})
     else:
@@ -81,7 +118,6 @@ uuid2, b))
             return 'Não autorizado!'
         except Exception as e:
             return jsonify({'error': str(e)})
-
 
 if __name__ == '__main__':
     app.run(
