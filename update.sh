e461e49df543b40724e5a56534b12768ebc060f3
Interceptv <97058207+Interceptv@users.noreply.github.com>
2023-09-17 14:42:39 -0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,34 @@
 #!/bin/bash
 versao_script="1.1"
+fun_bar() {
+		comando[0]="$1"
+		comando[1]="$2"
+		(
+			[[ -e $HOME/fim ]] && rm $HOME/fim
+			[[ ! -d /etc/SSHPlus ]] && rm -rf /bin/menu
+			${comando[0]} >/dev/null 2>&1
+			${comando[1]} >/dev/null 2>&1
+			touch $HOME/fim
+		) >/dev/null 2>&1 &
+		tput civis
+		echo -ne "\033[1;33mUPDATING \033[1;37m- \033[1;33m["
+		while true; do
+			for ((i = 0; i < 18; i++)); do
+				echo -ne "\033[1;31m#"
+				sleep 0.1s
+			done
+			[[ -e $HOME/fim ]] && rm $HOME/fim && break
+			echo -e "\033[1;33m]"
+			sleep 1s
+			tput cuu1
+			tput dl1
+			echo -ne "\033[1;33mUPDATING \033[1;37m- \033[1;33m["
+		done
+		echo -e "\033[1;33m]\033[1;37m -\033[1;32m OK !\033[1;37m"
+		tput cnorm
+	}
+clear
+
 # Função para verificar a atualização no arquivo JSON
 verificar_atualizacao() {
     # Coloque aqui o caminho completo para o seu arquivo JSON
@@ -24,42 +53,20 @@ verificar_atualizacao() {
         
         rm -r update.sh
         
+        clear
+        
         echo "Updating...."
         
-
-tarefa_demorada() {
-sleep 5
-    echo "Descompactando..."
-    sleep 5  # Simule uma tarefa que leva 5 segundos
-    echo "Ja estamos finalizando, aguarde"
-}
-
-# Função para exibir a barra de loading
-exibir_loading() {
-    local delay=0.1
-    local spinstr='|/-\'
-    local i=0
-
-    while [ ! -z "$(ps a | awk '{print $1}' | grep $1)" ]; do
-        i=$(( (i+1) %4 ))
-        printf "\r[%c] Aguarde..." "${spinstr:$i:1}"
-        sleep $delay
-    done
-    printf "\r"
-}
-
-# Executar tarefa demorada em segundo plano
-tarefa_demorada &
-PID=$!  # Obtenha o ID do processo em segundo plano
-
-# Exibir barra de loading
-exibir_loading $PID
-
-        sleep 2
         
+        updating() {
         wget "$linkupdate" && chmod 777 update.sh
+        }
         
-        sleep 5
+        sleep 3
+        
+        fun_bar 'updating'
+        
+        sleep 2
         
         mv novo_json.json "$arquivo_json"
         
